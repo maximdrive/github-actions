c700da1537b94c7ca28db948052c5c95f007aad3
Maxim Makarchuk <mmakarchuk@datamola.com>
2024-10-29 15:01:11 +0300

    hello

--- snake.js
+++ snake.js
@@ -21,7 +21,7 @@ function spawnFood() {
         y: Math.floor(Math.random() * (canvas.height / box)) * box
     };
 }
-   //hello
+   //hello world
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
